43675db7e8d63bbc1741d6a73facd1c073a27c4b
Efferusroxxa <efferusroxxa632@gmail.com>
2025-01-21 18:42:46 +0600

    Product_Details page Done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,95 @@
+// Fetch All Product
+// let products;
+const productsContainer = document.getElementById("products-container");
+
+fetch("https://fakestoreapi.com/products")
+.then(res => res.json())
+.then(products => {
+console.log(products);
+const productItems = products.map((product)=>{
+// console.log(product.rating.count)
+const productItemHTML =`
+   <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a href="product_details.html?productId=${product.id}" 
+              class="block 
+              relative h-48 
+              rounded 
+              overflow-hidden">
+                <img alt="ecommerce" 
+                class="object-cover 
+                object-center 
+                w-full h-full block" 
+                src="${product.image}">
+              </a>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                ${product.category}
+                </h3>
+                <h2 class="text-gray-900 title-font text-lg font-medium">
+                ${product.title}
+                </h2>
+                <p class="mt-1">${product.price}</p>
+              </div>
+            </div>
+   `;
+   return productItemHTML;
+ });
+ // console.log(productItems);
+// console.log(typeof productItems);
+const productsHTML = productItems.join(" ");
+console.log(productsHTML)
+productsContainer.innerHTML = productsHTML;
+})
+
+
+
+
+// Hamburger Menu
+const hambuergerBtn = document.getElementById("hamburger-menu");
+const mobileNavbar = document.getElementById("mobile-navbar");
+const clostBtn = document.getElementById("close-btn");
+
+
+// hambuergerBtn.addEventListener("click", function () {
+// // console.log("the hambergerbtn is clicked")
+// console.log(mobileNavbar.classList);
+// mobileNavbar.classList.remove("hidden")
+// mobileNavbar.classList.add("flex")
+// hambuergerBtn.classList.add("hidden")
+// clostBtn.classList.remove("hidden")
+// console.log(mobileNavbar.classList)
+// });
+
+hambuergerBtn.addEventListener("click", function () {
+    // console.log("the hambergerbtn is clicked")
+    mobileNavbar.classList.toggle("hidden")
+    mobileNavbar.classList.toggle("flex")
+    });
+
+
+clostBtn.addEventListener("click", function(){
+mobileNavbar.classList.add("hidden")
+mobileNavbar.classList.remove("flex")
+clostBtn.classList.add("hidden")
+hambuergerBtn.classList.add("block");
+hambuergerBtn.classList.remove("hidden");
+})
+// Hambuerger closed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log("Hello world im from jajabor")
 /**
  * 1.  shop button identify
@@ -9,8 +101,7 @@ const shopBtn1 = document.querySelector(".shop-btn-1");
 // console.log(shopBtn1);
 shopBtn1.addEventListener("click", function(){
     // console.log("This shop button is clicked");
-    console.log(window.location.href)
-    console.log("navigating to shop.html")
+
     window.location.href = "http://127.0.0.1:5500/shop.html";
     })
 
@@ -40,8 +131,7 @@ prevBtn.addEventListener("click", function () {
     const isFirstSlideActive = getComputedStyle (firstSlide).display == "flex";
     const isSecondSlideActive = getComputedStyle (secondSlide).display == "flex";
     const isThirdSlideActive = getComputedStyle(thirdSlide).display == "flex";
-    console.log(isFirstSlideActive, isSecondSlideActive, isThirdSlideActive);
-    console.log(isFirstSlideActive);
+
     if (isFirstSlideActive) {
     firstSlide.style.display = "none";
     secondSlide.style.display = "none";
@@ -79,7 +169,7 @@ prevBtn.addEventListener("click", function () {
         });
 
 
-        // console.log(getComputedStyle (secondSlide).display);
+     // console.log(getComputedStyle (secondSlide).display);
 /**
 *
 * 3 Slide -> Flex -> 2nd Slide none
